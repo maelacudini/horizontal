e3d57d2368cd313c52e00a696fedd5986fc732b2
maelacudini <maelacudini@gmail.com>
2024-02-27 15:06:29 +0100

    modifiche home

--- components/home/swpr/Swpr.jsx
+++ components/home/swpr/Swpr.jsx
@@ -4,8 +4,18 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Mousewheel } from "swiper/modules";
 import Slide from "../slide/Slide";
 import { projects, projectsVideo } from "@/utils/data";
+import { useEffect, useState } from "react";
 
 export default function Swpr() {
+  const [option, setOpion] = useState(true);
+  useEffect(() => {
+    if (window.innerWidth < 600) {
+      setOpion(false);
+    } else {
+      setOpion(true);
+    }
+  }, []);
+
   return (
     <section id="home">
       <Swiper
@@ -13,8 +23,8 @@ export default function Swpr() {
         spaceBetween={0}
         slidesPerView={"auto"}
         centeredSlides={true}
-        freeMode={true}
-        mousewheel={true}
+        freeMode={option}
+        mousewheel={option}
         draggable={true}
         loop={true}
         modules={[FreeMode, Mousewheel]}
